a7f9e943b6ee01e1600b82b93d8f1e74f10e1a6a
Ruben <106462316+moondevtech@users.noreply.github.com>
2022-12-31 13:32:08 +0200

    phonenuber check. - otp scroll design

--- MySavings-IOS/MySavings-IOS/Core/Extensions/View.swift
+++ MySavings-IOS/MySavings-IOS/Core/Extensions/View.swift
@@ -17,3 +17,15 @@ extension View {
 }
 #endif
 
+extension View {
+  @ViewBuilder func scrollEnabled(_ enabled: Bool) -> some View {
+    if enabled {
+      self
+    } else {
+      simultaneousGesture(DragGesture(minimumDistance: 0),
+                          including: .all)
+    }
+  }
+}
+
+
--- MySavings-IOS/MySavings-IOS/Presentation/AuthScreen/OtpScreen/OtpScreen.swift
+++ MySavings-IOS/MySavings-IOS/Presentation/AuthScreen/OtpScreen/OtpScreen.swift
@@ -8,165 +8,59 @@
 import SwiftUI
 import Combine
 
-struct OtpScreen: View {
 
-    enum FocusDigit : CaseIterable {
-        case one, two, three, four
-        var next : FocusDigit? {
-            switch self {
-            case .one:
-                return .two
-            case .two:
-                return .three
-            case .three:
-                return .four
-            case .four:
-                return .four
-            }
-        }
-        
-        var previous : FocusDigit? {
-            switch self {
-            case .one:
-                return .one
-            case .two:
-                return .one
-            case .three:
-                return .two
-            case .four:
-                return .three
-            }
-        }
-        
-    }
+protocol OtpViewScrolled {
+    var index : Int { get }
+}
+
+struct OtpScreen: View {
     
-    @State var phoneNumber : String = "0502894293"
+    let screenSize =  UIScreen.main.nativeBounds
     @State var textSearch : String = ""
+    @State var displayedViewId : Int = -1
     @StateObject var viewModel : OtpScreenViewModel = .init()
-    @FocusState var focusState : FocusDigit?
     
     var body: some View {
         
-        
         NavigationView {
             VStack{
-                Spacer()
-                HStack{
-                    CountryPickerNavigtor()
-                    VStack{
-                        TextField("", text: $phoneNumber)
-                            .foregroundColor(.black)
-                            .clipShape(Capsule())
-                            .frame(width: 200, height: 40, alignment : .center)
-
-                    }
-                    .frame(width: 240)
-                    .background(.white)
-                    .clipShape(Capsule())
-                }
-                
-                Spacer()
-                
-                HStack{
-                    if viewModel.digits.count > 0 {
-                        OneCaseField(digit: $viewModel.digits[0])
-                            .focused($focusState, equals: .one)
-                            .onAppear(perform: updateFocusState)
-                    }
+                GeometryReader { geo in
                     
-                    if viewModel.digits.count > 1 {
-                        OneCaseField(digit: $viewModel.digits[1])
-                            .focused($focusState, equals: .two)
-                            .onAppear(perform: updateFocusState)
-                    }
+                    let frame = geo.frame(in : .global)
                     
-                    if viewModel.digits.count > 2 {
-                        OneCaseField(digit: $viewModel.digits[2])
-                            .focused($focusState, equals: .three)
-                            .onAppear(perform: updateFocusState)
+                    ScrollView{
+                        ScrollViewReader{ reader in
+                            VStack{
+                                
+                                PhoneNumberChoiceView()
+                                    .frame(width: frame.width, height: frame.height  , alignment: .center)
+                                    .id(0)
+                                    .onAppear{
+                                        reader.scrollTo(0)
+                                    }
+                                
+                                
+                                OtpInputView()
+                                    .frame(width: frame.width, height: frame.height  , alignment: .center)
+                                    .id(1)
+                            }
+                            .frame(width: frame.width, height: frame.height * 2  , alignment: .center)
+                            .onReceive(viewModel.otpScreenScrollDestination) { destination in
+                                withAnimation(.spring()){
+                                    reader.scrollTo(destination)
+                                }
+                            }
+                        }
                     }
-                    if viewModel.digits.count > 3 {
-                        OneCaseField(digit:  $viewModel.digits[3])
-                            .focused($focusState, equals: .four)
-                            .onAppear(perform: updateFocusState)
-                    }
+                    .scrollEnabled(false)
+                    
                 }
-                
-                Spacer()
             }
-            .animation(.spring(), value: viewModel.digits)
-            .onReceive(viewModel.digitIndexToRemoveAnimateOut, perform: { index in
-                animateDigitOut(at: index)
-            })
-            .onReceive(viewModel.moveFirstDigitIn, perform: { animateDigitIn(at:$0)})
-            .preferredColorScheme(.dark)
-            .onAppear(perform: onViewAppeared)
+            .environmentObject(viewModel)
             .navigationBarTitle("")
         }
     }
     
-    @ViewBuilder
-    func CountryPickerNavigtor() -> some View {
-        NavigationLink {
-            OtpCountryPickerView()
-                .environmentObject(viewModel)
-        } label: {
-            Text("\(viewModel.selectedFlag!.emoji) \(viewModel.selectedFlag!.dial_code)")
-                .frame(width:80 ,height: 40, alignment : .center)
-                .background(.white)
-                .foregroundColor(.black)
-                .clipShape(Capsule())
-        }
-    }
-    
-    
-    @ViewBuilder
-    func OneCaseField(digit : Binding<DigitModel>) -> some View {
-        ZStack{
-            OtpFieldView(viewModel: viewModel)
-        }
-        .frame(width: 50, height: 50)
-        .foregroundColor(.black)
-        .background(Color.white)
-        .cornerRadius(25)
-        .offset(x:digit.wrappedValue.x)
-    }
-    
-    private func onChangeDigitValue( _ newValue : String) {
-        if newValue.isEmpty{
-            viewModel.handleInput(.animateOut)
-        }else{
-            print(newValue)
-            if newValue.count == 1 {
-                viewModel.handleInput(.digit(String(newValue.prefix(1))))
-            }else{
-                viewModel.handleInput(.remove)
-            }
-        }
-    }
-    
-    private func onViewAppeared(){
-        moveFirstDigit()
-    }
-    
-    private func moveFirstDigit(){
-        animateDigitIn(at: 0)
-        focusState = .one
-    }
-    
-    private func updateFocusState(){
-        if let next = focusState?.next{
-            focusState = next
-        }
-    }
-    
-    private func animateDigitIn(at index : Int){
-        viewModel.digits[index].x = 0
-    }
-    
-    private func animateDigitOut(at index : Int) {
-        viewModel.digits[index].x = CGFloat(300 * index)
-    }
 }
 
 struct OtpScreen_Previews: PreviewProvider {
